6b7111204695aca003d6f6e6c69c2963231bb3e5
Joseph M <jmedina1985@hotmail.com>
2021-12-06 17:18:49 -0800

    Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ import {
 } from '../components'
 
 const HomePage = () => {
-  
   return (
-    <h2>gatsby airtable starter</h2>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }
-
 
 export default HomePage
