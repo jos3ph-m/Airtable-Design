ec7865e7b3bb567555c0d3b7625cbd2453ac5930
Joseph M <jmedina1985@hotmail.com>
2021-12-11 19:27:41 -0800

    Projects component

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,13 +3,9 @@ import { graphql } from 'gatsby'
 import styled from 'styled-components'
 import { Layout, Projects, Algolia } from '../components'
 
-const ProjectsPage = () => {
-  
-  return (
-    <h2>projects page</h2>
-  )
+const ProjectsPage = ({ projects: data }) => {
+  return <h2>projects page</h2>
 }
-
 
 const Wrapper = styled.main`
   min-height: 100vh;
