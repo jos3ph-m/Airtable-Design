24706cb48012fabd63626f6dd2e647e71b9899d5
Joseph M <jmedina1985@hotmail.com>
2021-12-13 18:05:31 -0800

    Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,7 +26,9 @@ const Projects = ({ projects: data, title, page }) => {
                   alt={name}
                   className="img"
                 ></GatsbyImage>
-                <div className="info"></div>
+                <div className="info">
+                  <p>- {type} -</p>
+                </div>
               </div>
             </article>
           )
