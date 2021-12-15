83acebf2de8672d7fcf58e21366b200b87f1afa1
Joseph M <jmedina1985@hotmail.com>
2021-12-14 16:15:13 -0800

    Survey setup

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -9,7 +9,7 @@ const Survey = () => {
   const [loading, setLoading] = useState(true)
 
   const getRecords = async () => {
-    const records = await base('Survey').select({})
+    const records = await base('Survey').select({}).firstPage()
   }
 
   useEffect(() => {
