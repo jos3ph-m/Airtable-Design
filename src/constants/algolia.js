0f83a32cb86d4d351e10b89e3d4e7f3e2545021b
Joseph M <jmedina1985@hotmail.com>
2021-12-22 13:43:04 -0800

    Algolia

--- src/constants/algolia.js
+++ src/constants/algolia.js
@@ -1,4 +1,23 @@
 const airtableQuery = `
+{
+  allAirtable(filter: {table: {eq: "Projects"}}) {
+    nodes {
+      id
+      data {
+        date
+        name
+        type
+        image {
+          localFiles {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+}
 `
 
 const queries = [
