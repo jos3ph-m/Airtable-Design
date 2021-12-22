42ae8ef4970cd1d83cbcb1604a5d9559e6ab6ec6
Joseph M <jmedina1985@hotmail.com>
2021-12-21 17:09:51 -0800

    Algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,7 @@ module.exports = {
       resolve: `gatsby-plugin-algolia`,
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
       },
     },
   ],
