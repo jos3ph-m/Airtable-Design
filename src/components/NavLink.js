47d25f16e9de8b714dc0ee7b87a57b270f4a49dd
Joseph M <jmedina1985@hotmail.com>
2022-01-03 15:59:12 -0800

    Navbar sublinks

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -4,9 +4,8 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 import { GatsbyContext } from '../context/context'
 const NavLink = () => {
-  return (
-    <h2>nav link</h2>
-  )
+  const { links } = useContext(GatsbyContext)
+  return <h2>nav link</h2>
 }
 
 const Wrapper = styled.li`
